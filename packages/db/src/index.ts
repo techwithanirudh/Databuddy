a0cbdb988605e3f2ab866d8ebdff0ef1645b31a6
iza <59828082+izadoesdev@users.noreply.github.com>
2025-03-19 22:44:05 +0200

    fix: prisma

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,2 @@
 export { prisma as db } from './client' // exports instance of prisma 
-export * from "../generated/client" // exports generated types from prisma
+export * from "@prisma/client" // exports generated types from prisma

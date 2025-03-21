f9246f82a826ae4cbfbb1e0bef7f6dd9fcc5ad9d
iza <59828082+izadoesdev@users.noreply.github.com>
2025-03-21 03:37:04 +0200

    feat: fix prisma

--- apps/landing/types/prisma-plugin.d.ts
+++ apps/landing/types/prisma-plugin.d.ts
@@ -1,5 +1,5 @@
-// declare module '@prisma/nextjs-monorepo-workaround-plugin' {
-//   export class PrismaPlugin {
-//     constructor();
-//   }
-// } 
+declare module '@prisma/nextjs-monorepo-workaround-plugin' {
+  export class PrismaPlugin {
+    constructor();
+  }
+} 

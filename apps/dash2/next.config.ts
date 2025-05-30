e60cfee6247374933d8363029140dd7fefaa28b4
Hyteq <DerpsDer.es@gmail.com>
2025-05-30 22:10:01 +0300

    feat: remove sentry, its useless

--- apps/dash2/next.config.ts
+++ apps/dash2/next.config.ts
@@ -1,4 +1,3 @@
-import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -8,28 +7,4 @@ const nextConfig: NextConfig = {
 },
 };
 
-// export default nextConfig;
-
-export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-org: "databuddy",
-project: "javascript-nextjs",
-
-silent: !process.env.CI,
-telemetry: false,
-
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
-
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-});
+export default nextConfig;

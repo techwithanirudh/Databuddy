ec28997ba431d91da5e123230f2849c34b031abd
Hyteq <DerpsDer.es@gmail.com>
2025-06-07 09:41:45 +0300

    credit to betterauth

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -49,6 +49,9 @@ export default async function HomePage() {
 							</div>
 						</div>
 						<div className="flex items-center gap-4 text-xs text-neutral-500">
+              <span>
+                <a href="https://www.better-auth.com" className="underline">Better-auth</a> inspired the design.
+              </span>
 							{/* <span className="flex items-center gap-1">
 								<svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20" aria-label="Checkmark" title="Checkmark">
 									<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />

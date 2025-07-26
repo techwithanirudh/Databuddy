5ee237dd5a38fc0818624623a2b758e07d103a60
Hyteq <DerpsDer.es@gmail.com>
2025-07-26 13:53:13 +0300

    just add endpoint

--- apps/basket/src/lib/logger.ts
+++ apps/basket/src/lib/logger.ts
@@ -1,9 +1,8 @@
 import { Logtail } from '@logtail/edge';
 
 const token = process.env.LOGTAIL_SOURCE_TOKEN as string;
-const endpoint = process.env.LOGTAIL_ENDPOINT as string;
 export const logger = new Logtail(token, {
-	endpoint,
+	endpoint: 's1447431.eu-nbg-2.betterstackdata.com',
 	batchSize: 10,
 	batchInterval: 1000,
 });

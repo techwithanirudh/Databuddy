e99bc0d2d9b6277b16395157647ec305cd2c9ef7
Hyteq <DerpsDer.es@gmail.com>
2025-07-26 16:03:01 +0300

    social pages

---
+++ apps/docs/app/(socials)/discord/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function DiscordPage() {
+	redirect('https://discord.gg/JTk7a38tCZ');
+}
---
+++ apps/docs/app/(socials)/twitter/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function TwitterPage() {
+	redirect('https://x.com/trydatabuddy');
+}

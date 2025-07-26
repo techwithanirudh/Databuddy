7fcd573cc8229c1b37aeca5a613ba12e4bb45cd7
Hyteq <DerpsDer.es@gmail.com>
2025-07-26 13:55:58 +0300

    pino logger

--- apps/basket/src/lib/logger.ts
+++ apps/basket/src/lib/logger.ts
@@ -1,29 +1,14 @@
-import { Logtail } from '@logtail/edge';
+import pino from 'pino';
 
-const token = process.env.LOGTAIL_SOURCE_TOKEN as string;
-export const logger = new Logtail(token, {
-	endpoint: 's1447431.eu-nbg-2.betterstackdata.com',
-	batchSize: 10,
-	batchInterval: 1000,
+const logger = pino({
+	level: 'info',
+	transport: {
+		target: '@logtail/pino',
+		options: {
+			sourceToken: process.env.LOGTAIL_SOURCE_TOKEN,
+			options: { endpoint: 'https://s1447431.eu-nbg-2.betterstackdata.com' },
+		},
+	},
 });
 
-// Log levels to ensure we only log important events
-// export enum LogLevel {
-//     ERROR = 'error',
-//     WARN = 'warn',
-//     INFO = 'info',
-//     DEBUG = 'debug'
-// }
-
-// const log = (level: LogLevel, message: string, data?: any) => {
-//     logger.log(level, message, data);
-// };
-
-// const pinoLogger = pino({
-//     level: 'info',
-//     transport: {
-//         target: 'pino-pretty',
-//     },
-// });
-
-// export { pinoLogger as logger };
+export { logger };

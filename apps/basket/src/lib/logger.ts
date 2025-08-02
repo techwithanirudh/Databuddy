9b625f62b7f678dbcad05ede0a3de6923811e3be
Hyteq <DerpsDer.es@gmail.com>
2025-08-02 10:13:59 +0300

    fix loggers

--- apps/basket/src/lib/logger.ts
+++ apps/basket/src/lib/logger.ts
@@ -2,8 +2,8 @@ import { Logtail } from '@logtail/edge';
 
 const token = process.env.LOGTAIL_SOURCE_TOKEN as string;
 const endpoint = process.env.LOGTAIL_ENDPOINT as string;
-export const logger = new Logtail(token, {
-	endpoint,
+export const logger = new Logtail(token || '', {
+	endpoint: endpoint || '',
 	batchSize: 10,
 	batchInterval: 1000,
 });

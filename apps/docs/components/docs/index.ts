e02e0d61a482226f7b24b1313f3e9fc14f674214
Hyteq <DerpsDer.es@gmail.com>
2025-08-27 14:33:54 +0300

    fix: monthly pricing

--- apps/docs/components/docs/index.ts
+++ apps/docs/components/docs/index.ts
@@ -1,30 +1,29 @@
 // Callout component
 
+export {
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
 // Accordion components
 export {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  Accordions,
-  AccordionTrigger,
-} from "./accordion";
-export { Callout } from "./callout";
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	Accordions,
+	AccordionTrigger,
+} from './accordion';
+export { Callout } from './callout';
 // Card components
 export {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  Cards,
-  CardTitle,
-} from "./card";
+	Card,
+	Cards,
+} from './card';
 // Code components
-export { CodeBlock, InlineCode } from "./code-block";
-
+export { CodeBlock, InlineCode } from './code-block';
+export { LeapComponent } from './leap-component';
 // Steps components
-export { CompletedStep, Step, Steps } from "./steps";
-
+export { CompletedStep, Step, Steps } from './steps';
 // Tabs components
-export { Tab, Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
-
-export { LeapComponent } from "./leap-component";
+export { Tab, Tabs, TabsContent, TabsList, TabsTrigger } from './tabs';

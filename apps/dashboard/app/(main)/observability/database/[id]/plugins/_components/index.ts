8adc5c9ff77d69156abb0b7d5a9c2f606dde38f5
Hyteq <DerpsDer.es@gmail.com>
2025-09-19 21:17:07 +0300

    fix build issue :skull:

--- apps/dashboard/app/(main)/observability/database/[id]/plugins/_components/index.ts
+++ apps/dashboard/app/(main)/observability/database/[id]/plugins/_components/index.ts
@@ -1,5 +1,4 @@
 export { ExtensionCard } from './extension-card';
-export { ExtensionEmptyState } from './extension-empty-state';
 export { ExtensionSearch } from './extension-search';
 export { ExtensionStats } from './extension-stats';
 export { ExtensionTabs } from './extension-tabs';
